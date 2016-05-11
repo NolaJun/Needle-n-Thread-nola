b7b0ad903eef6f9ef6379b68cc37ec52e9fcd10c
wulj <wulj@yunys.com.cn>
2016-05-11 16:46:17 +0800

    .typo

--- js/comment.js
+++ js/comment.js
@@ -2,12 +2,39 @@
 * @Author: yys_wlj
 * @Date:   2016-05-11 15:26:19
 * @Last Modified by:   yys_wlj
-* @Last Modified time: 2016-05-11 15:36:03
+* @Last Modified time: 2016-05-11 16:44:27
 */
 
 'use strict';
 $(document).ready(function() {
-	$('.select-num a .plus').click(function() {
-		$(this).parents('.select-num a').children().show();
+	$('aside ul li').click(function() {
+		$(this).siblings().removeClass('actived');
+		$(this).addClass('actived');
 	});
+	$('.select-num a .plus').click(function() {
+		$(this).parent().siblings().children().show();
+	});
+	var numbers = $('numbers').val();
+	if($('.select-num a .sub,.select-num a .numbers').attr('display',"block")){
+		addNum();
+		subNum();
+	}
 });
+function addNum(){
+	var numbers = $('.number').val();
+	$('.plus').click(function() {
+		numbers ++;
+		$('.number').text(numbers);
+	});
+}
+function subNum(){
+	var numbers = $('.number').val();
+	$('.sub').click(function() {
+		if(numbers >= 1){
+			numbers --;
+		}else{
+			numbers = 0;
+		}
+		$('.number').text(numbers);
+	});
+}
