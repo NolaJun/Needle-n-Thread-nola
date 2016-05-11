a805d08c89b40ed8fba579983bf122f5f0a9f9ce
wulj <wulj@yunys.com.cn>
2016-05-11 17:03:25 +0800

    .typo

--- js/comment.js
+++ js/comment.js
@@ -2,7 +2,7 @@
 * @Author: yys_wlj
 * @Date:   2016-05-11 15:26:19
 * @Last Modified by:   yys_wlj
-* @Last Modified time: 2016-05-11 16:44:27
+* @Last Modified time: 2016-05-11 17:01:22
 */
 
 'use strict';
@@ -10,25 +10,29 @@ $(document).ready(function() {
 	$('aside ul li').click(function() {
 		$(this).siblings().removeClass('actived');
 		$(this).addClass('actived');
+		var t = $(this).index();
+		$('article').index(t).addClass('default-show').siblings().hide();
 	});
 	$('.select-num a .plus').click(function() {
 		$(this).parent().siblings().children().show();
 	});
 	var numbers = $('numbers').val();
 	if($('.select-num a .sub,.select-num a .numbers').attr('display',"block")){
-		addNum();
-		subNum();
+		var numbers = $('.number').val();
+		addNum(numbers);
+		subNum(numbers);
 	}
 });
-function addNum(){
-	var numbers = $('.number').val();
+function addNum(numbers){
+	// var numbers = $('.number').val();
 	$('.plus').click(function() {
 		numbers ++;
 		$('.number').text(numbers);
 	});
+	return numbers;
 }
-function subNum(){
-	var numbers = $('.number').val();
+function subNum(numbers){
+	// var numbers = $('.number').val();
 	$('.sub').click(function() {
 		if(numbers >= 1){
 			numbers --;
